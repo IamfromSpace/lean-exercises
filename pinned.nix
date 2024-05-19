a9f6c805493cc82563ec5ff01afd42f3c1d05cab
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2024-05-19 16:48:47 -0700

    Use a tag and a content hash check to pinned deps.

--- pinned.nix
+++ pinned.nix
@@ -1,6 +1,7 @@
 import
   (builtins.fetchTarball {
     name = "nixos-23.11";
-    url = "https://github.com/nixos/nixpkgs/archive/04220ed6763637e5899980f98d5c8424b1079353.tar.gz";
+    url = "https://github.com/nixos/nixpkgs/archive/refs/tags/23.11.tar.gz";
+    sha256 = "sha256:1ndiv385w1qyb3b18vw13991fzb9wg4cl21wglk89grsfsnra41k";
   })
   {}
