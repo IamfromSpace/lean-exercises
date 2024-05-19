6b49aaf6bf8760031a373b8075963b7a378c0559
Nathan Fairhurst <nathan.p3pictures@gmail.com>
2024-05-19 16:47:33 -0700

    Re-organize files into a more typical lean project structure.

---
+++ Examples.lean
@@ -0,0 +1,6 @@
+import «Examples».BarbersParadox
+import «Examples».InductionAndRecursion
+import «Examples».InductiveTypes
+import «Examples».MikesChallenge
+import «Examples».Proposition
+import «Examples».Quantifiers
